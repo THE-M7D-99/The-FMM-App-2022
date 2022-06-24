b34e3c7773bdc41435f6b9c84f71a08c4bf2615d
Osama Alathwari <osamaz1x13@gmail.com>
2022-06-24 19:00:45 +0000

    Login Frame By Osama Alathwari

---
+++ FMM/FMM-Application/App/UIs/Permissions/OS/Login/Login_Form.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace FMM_Application.App.UIs.Permissions.OS.Login
+{
+    partial class Login_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login_Form));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Btn_OK = new DevExpress.XtraEditors.SimpleButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.text_pass = new System.Windows.Forms.TextBox();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.text_user = new System.Windows.Forms.TextBox();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(20);
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel1.Size = new System.Drawing.Size(618, 81);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(20);
+            this.label1.Size = new System.Drawing.Size(159, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "تسجيل الدخول";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.Btn_OK);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 782);
+            this.panel2.Name = "panel2";
+            this.panel2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel2.Size = new System.Drawing.Size(618, 89);
+            this.panel2.TabIndex = 1;
+            // 
+            // Btn_OK
+            // 
+            this.Btn_OK.Appearance.BackColor = System.Drawing.SystemColors.Control;
+            this.Btn_OK.Appearance.BackColor2 = System.Drawing.SystemColors.Control;
+            this.Btn_OK.Appearance.BorderColor = System.Drawing.Color.White;
+            this.Btn_OK.Appearance.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_OK.Appearance.Options.UseBackColor = true;
+            this.Btn_OK.Appearance.Options.UseBorderColor = true;
+            this.Btn_OK.Appearance.Options.UseFont = true;
+            this.Btn_OK.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Btn_OK.Enabled = false;
+            this.Btn_OK.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("Btn_OK.ImageOptions.SvgImage")));
+            this.Btn_OK.Location = new System.Drawing.Point(0, 0);
+            this.Btn_OK.Name = "Btn_OK";
+            this.Btn_OK.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Btn_OK.Size = new System.Drawing.Size(618, 89);
+            this.Btn_OK.TabIndex = 0;
+            this.Btn_OK.Text = "تسجيل الدخول";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 81);
+            this.panel3.Name = "panel3";
+            this.panel3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel3.Size = new System.Drawing.Size(618, 701);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.panel7);
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(0, 363);
+            this.panel5.Name = "panel5";
+            this.panel5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel5.Size = new System.Drawing.Size(618, 338);
+            this.panel5.TabIndex = 1;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.panel11);
+            this.panel7.Controls.Add(this.panel10);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel7.Location = new System.Drawing.Point(0, 142);
+            this.panel7.Name = "panel7";
+            this.panel7.Padding = new System.Windows.Forms.Padding(10);
+            this.panel7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel7.Size = new System.Drawing.Size(618, 196);
+            this.panel7.TabIndex = 3;
+            // 
+            // panel11
+            // 
+            this.panel11.Controls.Add(this.text_pass);
+            this.panel11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel11.Location = new System.Drawing.Point(10, 72);
+            this.panel11.Name = "panel11";
+            this.panel11.Padding = new System.Windows.Forms.Padding(15, 10, 10, 10);
+            this.panel11.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel11.Size = new System.Drawing.Size(598, 114);
+            this.panel11.TabIndex = 3;
+            // 
+            // text_pass
+            // 
+            this.text_pass.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.text_pass.Location = new System.Drawing.Point(15, 10);
+            this.text_pass.Margin = new System.Windows.Forms.Padding(20);
+            this.text_pass.Name = "text_pass";
+            this.text_pass.PasswordChar = '*';
+            this.text_pass.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.text_pass.Size = new System.Drawing.Size(573, 27);
+            this.text_pass.TabIndex = 2;
+            this.text_pass.TextChanged += new System.EventHandler(this.text_pass_TextChanged);
+            this.text_pass.KeyDown += new System.Windows.Forms.KeyEventHandler(this.text_pass_KeyDown);
+            // 
+            // panel10
+            // 
+            this.panel10.Controls.Add(this.label3);
+            this.panel10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel10.Location = new System.Drawing.Point(10, 10);
+            this.panel10.Name = "panel10";
+            this.panel10.Padding = new System.Windows.Forms.Padding(10);
+            this.panel10.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel10.Size = new System.Drawing.Size(598, 62);
+            this.panel10.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Location = new System.Drawing.Point(423, 10);
+            this.label3.Margin = new System.Windows.Forms.Padding(5);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(30);
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(165, 79);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "كلمة المرور :";
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.panel9);
+            this.panel6.Controls.Add(this.panel8);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel6.Location = new System.Drawing.Point(0, 0);
+            this.panel6.Margin = new System.Windows.Forms.Padding(5);
+            this.panel6.Name = "panel6";
+            this.panel6.Padding = new System.Windows.Forms.Padding(10);
+            this.panel6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel6.Size = new System.Drawing.Size(618, 215);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel9
+            // 
+            this.panel9.Controls.Add(this.text_user);
+            this.panel9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel9.Location = new System.Drawing.Point(10, 76);
+            this.panel9.Name = "panel9";
+            this.panel9.Padding = new System.Windows.Forms.Padding(15, 10, 10, 10);
+            this.panel9.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel9.Size = new System.Drawing.Size(598, 129);
+            this.panel9.TabIndex = 2;
+            // 
+            // text_user
+            // 
+            this.text_user.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.text_user.Location = new System.Drawing.Point(15, 10);
+            this.text_user.Margin = new System.Windows.Forms.Padding(20);
+            this.text_user.Name = "text_user";
+            this.text_user.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.text_user.Size = new System.Drawing.Size(573, 27);
+            this.text_user.TabIndex = 1;
+            this.text_user.TextChanged += new System.EventHandler(this.text_user_TextChanged);
+            this.text_user.KeyDown += new System.Windows.Forms.KeyEventHandler(this.text_user_KeyDown);
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.label2);
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel8.Location = new System.Drawing.Point(10, 10);
+            this.panel8.Margin = new System.Windows.Forms.Padding(10);
+            this.panel8.Name = "panel8";
+            this.panel8.Padding = new System.Windows.Forms.Padding(10);
+            this.panel8.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel8.Size = new System.Drawing.Size(598, 66);
+            this.panel8.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Location = new System.Drawing.Point(393, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(5);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(30);
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(195, 79);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "اسم المستخدم :";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel4.Size = new System.Drawing.Size(618, 363);
+            this.panel4.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.pictureBox1.Size = new System.Drawing.Size(618, 363);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            this.errorProvider1.RightToLeft = true;
+            // 
+            // Login_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(618, 871);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login_Form";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "تسجيل الدخول";
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel11.ResumeLayout(false);
+            this.panel11.PerformLayout();
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.TextBox text_user;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.TextBox text_pass;
+        private System.Windows.Forms.Panel panel10;
+        private DevExpress.XtraEditors.SimpleButton Btn_OK;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ FMM/FMM-Application/App/UIs/Permissions/OS/Login/Login_Form.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FMM_Application.App.UIs.Permissions.OS.Login
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        public void Login_data()
+        {
+            errorProvider1.Clear();
+            errorProvider1.Clear();
+            if (this.text_user.Text.Trim() != "" && this.text_pass.Text.Trim() != "")
+            {
+                // Database Connection Codes and sql queries
+
+
+
+
+
+            }
+
+            else
+            {
+                if (this.text_pass.Text.Trim() == "")
+                    errorProvider1.SetError(text_pass, "Password Can not be Empty");
+
+                if (this.text_user.Text.Trim() == "")
+                    errorProvider1.SetError(text_user, "Username Can not be Empty");
+            }
+        }
+
+        private void text_user_TextChanged(object sender, EventArgs e)
+        {
+            if (this.text_user.Text.Trim() != "" && this.text_pass.Text.Trim() != "")
+                Btn_OK.Enabled = true;
+
+            else
+                Btn_OK.Enabled = false;
+        }
+
+        private void text_pass_TextChanged(object sender, EventArgs e)
+        {
+            if (this.text_user.Text.Trim() != "" && this.text_pass.Text.Trim() != "")
+                Btn_OK.Enabled = true;
+
+            else
+                Btn_OK.Enabled = false;
+        }
+
+        private void text_user_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Down)
+                this.text_pass.Focus();
+
+            if (e.KeyData == Keys.Enter)
+                this.text_pass.Focus();
+        }
+
+        private void text_pass_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Up)
+                this.text_user.Focus();
+
+            if (e.KeyData == Keys.Enter)
+                Login_data();
+        }
+    }
+}
